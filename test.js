3cb90336c07a0b7f07fdbc210d1e8e71367a35db
larimuto <larimuto@gmail.com>
2023-12-28 17:20:59 +0200

    softuni-Js-path

--- test.js
+++ test.js
@@ -1,45 +1,22 @@
 function manipulateArray(inputArray, commands) {
-
-    let  list =[ {
-        Petur : {gosho : 23} ,
-        Stoyo : {gosho : {trinidad : "hello"}}
-
-
-
-
-
+  let list = [
+    {
+      Petur: { gosho: 23 },
+      Stoyo: { gosho: { trinidad: "hello" } },
     },
     {
-        Petur : {gosho : 23} ,
-        Stoyo : {gosho : {trinidad : "hello"}}
+      Petur: { gosho: 23 },
+      Stoyo: { gosho: { trinidad: "hello" } },
+    },
+  ];
 
+  console.log(list);
 
-
-
-
-    }
-]
-
-console.log(list);
-
-
-for (let element of Object.entries(list)) {
+  for (let element of Object.entries(list)) {
     // console.log(Object.entries(element[1])[0][1]);
     // console.log(element);
     // console.log(Object.values(element)[1]);
-}
-
-
-
-
-
-
-
-
-
-
-
-
+  }
 }
 manipulateArray(
   [1, 2, 4, 5, 6, 7],
