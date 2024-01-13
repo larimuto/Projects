3f076f93808dbaddd312d135b957ba17626e4182
larimuto <larimuto@gmail.com>
2024-01-13 22:45:03 +0200

    new work

--- Js-Advanced/Advanced-Module/04. DOM introduction - exercise/03. Accordion/accordion.js
+++ Js-Advanced/Advanced-Module/04. DOM introduction - exercise/03. Accordion/accordion.js
@@ -1,3 +1,14 @@
 function toggle() {
-    console.log('TODO:...');
+    // take the  button  name AS state .
+  let state = document.querySelector(".head .button").textContent.toLowerCase();
+  // take the element that hides the text for manipulations
+  let showMore = document.getElementById("extra");
+//check the state and exchange the  disolay and switch the  textcontent of  the button element
+  if (state === "more") {
+    document.querySelector(".head .button").textContent = "Less";
+    showMore.style.display = "block";
+  } else {
+    document.querySelector(".head .button").textContent = "More";
+    showMore.style.display = "none";
+  }
 }
