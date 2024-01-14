25aafca0a607df4fa964e87b2a538ef94d5fdb03
larimuto <larimuto@gmail.com>
2024-01-14 23:56:48 +0200

    new work

--- Js-Advanced/Advanced-Module/04. DOM introduction - exercise/05. Table - Search Engine/solution.js
+++ Js-Advanced/Advanced-Module/04. DOM introduction - exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,24 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
 
-   function onClick() {
-      //   TODO:
+  function onClick() {
+    let result = document.getElementById("result");
+    let command = document.getElementById("searchField").value;
+    let xTable = document.querySelectorAll("table.container tbody tr");
+    let condition = false;
 
-   }
+    for (let row = 0; row < xTable.length; row++) {
+      mainCell = xTable[row];
+
+      for (const cell of Array.from(mainCell.querySelectorAll("td"))) {
+        state = cell.textContent.toLowerCase().includes(command.toLowerCase());
+        if (state && command !== "") {
+          condition = true;
+          mainCell.classList.add("select");
+        } else if (command === "" || condition === false) {
+          mainCell.classList.remove("select");
+        }
+      }
+    }
+  }
 }
