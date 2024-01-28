699f6e25fbac7038075c353dd3ecd1cb1f6911f9
larimuto <larimuto@gmail.com>
2024-01-28 22:57:15 +0200

    new work

--- Js-Advanced/Advanced-Module/05.JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
+++ Js-Advanced/Advanced-Module/05.JS-Advanced-DOM-Events-Exercise-Resources/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+  const text = document.querySelector("#newItemText");
+  const curValue = document.querySelector("#newItemValue");
+  const menu = document.querySelector("#menu");
+  // make the new element
+  let newOption = document.createElement("option");
+  newOption.textContent = text.value;
+  newOption.value = curValue.value;
+  // append the new element
+  menu.appendChild(newOption);
+  // reset state
+  text.value = "";
+  curValue.value = "";
 }
